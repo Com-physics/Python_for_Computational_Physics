16b4709bb34596267be2218d839054891bfde571
Com-physics <142432551+Com-physics@users.noreply.github.com>
2024-02-04 14:53:42 +0000

    sin_wave_covergence.py

--- sin_wave_covergence.py
+++ sin_wave_covergence.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.arange(0, 50, 0.0001) # Declaring x as a variable and assigning the value with the help of numpy arrange function starting from 0 to ending at 50 with the step value = 0.0001 (for smoother wave plotting)
+x = np.arange(0, 50, 0.0001) # Declaring x as a variable and assigning the value with the help of numpy arange function starting from 0 to ending at 50 with the step value = 0.0001 (for smoother wave plotting)
 sin1 = np.sin(x) # Using numpy module assign f(x) = sin(x) as a wave 1
 sin2 = np.sin(2*x) # Similiarly assign g(x) = sin(2x) as a wave 2
 y= sin1 + sin2 # To visualize the convergence, h(x) = f(x) + g(x)
